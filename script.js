5121c48983d208183e6d551bbca6faacd1cad63d
Confused-Owp <Owlconfused@gmail.com>
2023-05-23 18:56:39 +0500

    Return board as object

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
 (function gameBoard() {
-  function divAdd(array) {
+  let board = [];
+
+  (function divAdd(array) {
     for (let i = 1; i <= 9; i++) {
       array.push("<div> </div>");
     }
-  }
-  let gameBoard = {
-    board: [],
-  };
-  gameBoard.divAdd = (function (board) {
-    divAdd(board);
-  })(gameBoard.board);
-  console.log(gameBoard.board);
-  return {
-    board: gameBoard.board,
-  };
+  })(board);
+
+  return { board };
 })();
 console.log(gameBoard.board);
