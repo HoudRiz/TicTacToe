36794acc90665e42b350afc247d57d0d56b1b4a4
Confused-Owp <Owlconfused@gmail.com>
2023-05-30 17:02:55 +0500

    Add personfactory

--- script.js
+++ script.js
@@ -3,10 +3,20 @@ let gameBoard = (function () {
 
   (function divAdd(array) {
     for (let i = 1; i <= 9; i++) {
-      array.push("<div> </div>");
+      array.push("");
     }
   })(board);
 
   return { board };
 })();
+
+let personFactory = function (name, indicator) {
+  return {
+    name,
+    indicator,
+  };
+};
+
+let player1 = personFactory("player1", "X");
+console.log(player1.name);
 console.log(gameBoard.board);
