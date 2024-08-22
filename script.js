2ab5f60beee73ff49c963c41cd2c8c14891f074b
HoudRiz <122109972+HoudRiz@users.noreply.github.com>
2024-08-22 08:27:06 +0500

    Add missing win condition

--- script.js
+++ script.js
@@ -36,9 +36,10 @@ function findIndicesWithOne() {
 function checkCombination(numbers) {
   const winningCombinations = [
     [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
     [1, 4, 7],
     [3, 6, 9],
-    [7, 8, 9],
     [1, 5, 9],
     [3, 5, 7],
   ];
