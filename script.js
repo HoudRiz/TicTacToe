b970629e034f86ce2bdd5fd3a103d90cf0976f0d
HoudRiz <122109972+HoudRiz@users.noreply.github.com>
2024-08-22 06:01:35 +0500

    Remove gameBoard function

--- script.js
+++ script.js
@@ -1,21 +1,8 @@
-let gameBoard = (function () {
-  let board = [];
-
-  (function divAdd(array) {
-    for (let i = 1; i <= 9; i++) {
-      array.push("");
-    }
-  })(board);
-
-  return { board };
-})();
-
-let personFactory = function (name, indicator) {
+const personFactory = function (name, indicator) {
   return {
     name,
     indicator,
   };
 };
 
-let player1 = personFactory("player1", "X");
-
+const player1 = personFactory('player1', 'X');
